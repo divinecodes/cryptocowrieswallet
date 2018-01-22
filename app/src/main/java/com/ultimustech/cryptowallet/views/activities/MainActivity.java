7c4ddc5510f52f2946d3c50a231ad01ec59a93f6
poacher <tetteydivine0@gmail.com>
2018-01-22 20:24:06 +0000

    Setting up Navigation Fragments

--- app/src/main/java/com/ultimustech/cryptowallet/views/activities/MainActivity.java
+++ app/src/main/java/com/ultimustech/cryptowallet/views/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ultimustech.cryptowallet;
+package com.ultimustech.cryptowallet.views.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.ultimustech.cryptowallet.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
