e43ba1eca975c8b5335a1ef7be87ca3a42d61fc5
poacher <tetteydivine0@gmail.com>
2018-01-30 06:38:53 +0000

    BugFix

--- app/src/main/java/com/ultimustech/cryptowallet/views/fragments/AccountFragment.java
+++ app/src/main/java/com/ultimustech/cryptowallet/views/fragments/AccountFragment.java
@@ -23,8 +23,10 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View accountView = inflater.inflate(R.layout.fragment_account,container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        return accountView;
     }
 
 }
